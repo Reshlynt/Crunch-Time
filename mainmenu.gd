2304b02bce87f9de81c84c04ddb571a0ea085cc3
jkolbusz@email.sc.edu <Crunko>
2024-11-21 22:14:14 -0500

    added pause menu

--- mainmenu.gd
+++ mainmenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_poc_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/maps/infinite_runner.tscn")
+	get_tree().change_scene_to_file("res://tutorial-endless-run/art/scene/main.tscn")
 
 
 func _on_play_demo_pressed() -> void:
