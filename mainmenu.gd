9fce20e218af38f74a7bb1bb3c5d0c52fa10ced1
jkolbusz@email.sc.edu <Crunko>
2024-12-13 00:15:08 -0500

    added how to play and about game to main menu

--- mainmenu.gd
+++ mainmenu.gd
@@ -13,3 +13,11 @@ func _on_play_demo_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_about_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://tutorial-endless-run/art/scene/about_game.tscn")
+
+
+func _on_how_to_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://tutorial-endless-run/art/scene/how_to_play.tscn")
