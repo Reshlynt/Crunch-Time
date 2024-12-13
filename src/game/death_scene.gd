823ff3d61ae4009cad6a0248257368e52c812927
Scott Do <dobao98123@gmail.com>
2024-12-13 13:26:50 -0500

    import score system and fix death script

--- src/game/death_scene.gd
+++ src/game/death_scene.gd
@@ -1,14 +1,12 @@
 extends Control
 
-@onready var main = $"../"
-
-
+# Return to main game.
 func _on_retry_pressed() -> void:
-	get_tree().change_scene_to_file("res://tutorial-endless-run/art/scene/main.tscn")
+	get_tree().change_scene_to_file("res://game-scene/game/main-game.tscn")
 
-
+# Returns to menu
 func _on_back_to_main_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://field/scenes/maps/mainmenu.tscn")
+	get_tree().change_scene_to_file("res://game-scene/game/mainmenu.tscn")
 
 
 func _on_quit_pressed() -> void:
